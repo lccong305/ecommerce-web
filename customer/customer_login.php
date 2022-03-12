420514901d27e99033d8d11d27ba0ac4b9df8c43
An <hoangan200150@gmail.com>
2022-03-12 09:38:03 +0700

    CA-8: User login update

--- customer/customer_login.php
+++ customer/customer_login.php
@@ -11,14 +11,6 @@
 
 </center>
 
-<p class="text-muted" >
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-
-</p>
-
-
-
-
 </div><!-- box-header Ends -->
 
 <form action="checkout.php" method="post" ><!--form Starts -->
